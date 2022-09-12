7dcfd1fdf353c7c8301248cc6bbca59acd16e16c
thirumasd <113158761+thirumasd@users.noreply.github.com>
2022-09-12 12:45:56 +0530

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,6 +4,6 @@ set :bind, '0.0.0.0'
 set :port, ENV['PORT'] || '8080'
 
 get '/' do
-  target = ENV['TARGET'] || 'World'
+  target = ENV['TARGET'] || 'World - Ruby sample app'
   "Hello #{target}!\n"
 end
